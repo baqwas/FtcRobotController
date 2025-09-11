05c65ce46d033300337870f04150e19b312843ad
baqwas <baqwas@gmail.com>
2025-09-11 15:33:32 -0500

    Final push prior to DECODE sync

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Control/BangBangController.java
@@ -0,0 +1,168 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode.Control;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Datalogger.Datalogger;
+
+/**
+ * This OpMode demonstrates a simple bang-bang control system for a flywheel motor,
+ * with enhanced data logging to analyze performance using a structured Datalogger utility
+ * Bang-bang control is a very simple form of feedback control where the actuator
+ * is either fully on or fully off, depending on whether a setpoint is met.
+ * This example uses the concept of a "target velocity" for the flywheel. The code
+ * will continuously check the motor's current velocity. If the current velocity
+ * is below the target, the motor will be set to full power (1.0). If the current
+ * velocity is at or above the target, the motor will be turned off (0.0).
+ * This is a simple but effective way to maintain a target speed.
+ * For more advanced and stable control, you would use a PID controller.
+ */
+@TeleOp(name = "Bang-Bang Control", group = "Controller")
+public class BangBangController extends OpMode {
+
+    // Declare a motor object. We use DcMotorEx for velocity control.
+    private DcMotorEx flywheelMotor = null;
+
+    // Define the target velocity in ticks per second.
+    // FTCRobotController SDK typically uses encoder ticks for velocity.
+    // You will need to determine the appropriate value for your specific motor and gear ratio.
+    private final double TARGET_VELOCITY = 2000; // Example target in ticks/sec
+
+    // A timer to display some information on the telemetry.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Data logging variables using the provided Datalogger utility.
+    private Datalog datalog;
+
+    @Override
+    public void init() {
+        // Get the motor from the hardware map.
+        flywheelMotor = hardwareMap.get(DcMotorEx.class, "motorFlywheel");
+
+        // Set the motor direction. You might need to change this depending on your robot's wiring.
+        flywheelMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // This is crucial for velocity control. The motor will use the built-in
+        // encoder to try and maintain a set velocity.
+        flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Set the zero power behavior to BRAKE. This helps the motor stop faster.
+        flywheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Initialize the new Datalogger
+        datalog = new Datalog("FlywheelBangBang_Data");
+        datalog.opModeStatus.set("Initialized");
+        datalog.targetVelocity.set(TARGET_VELOCITY);
+        datalog.writeLine();
+
+        // Tell the user everything is ready.
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Target Velocity", "%.2f ticks/sec", TARGET_VELOCITY);
+    }
+
+    @Override
+    public void loop() {
+        // Get the current velocity of the flywheel motor.
+        double currentVelocity = flywheelMotor.getVelocity();
+        double motorPower;
+
+        // Implement the bang-bang control logic.
+        if (currentVelocity < TARGET_VELOCITY) {
+            // If the current velocity is below the target, turn the motor on at full power.
+            motorPower = 1.0;
+            datalog.opModeStatus.set("Running (motor ON)");
+        } else {
+            // If the current velocity is at or above the target, turn the motor off.
+            motorPower = 0.0;
+            datalog.opModeStatus.set("Running (motor OFF)");
+        }
+
+        flywheelMotor.setPower(motorPower);
+
+        // Update and log the data using the Datalog utility.
+        datalog.currentVelocity.set(currentVelocity);
+        datalog.motorPower.set(motorPower);
+        datalog.writeLine();
+
+        // Add useful information to the telemetry.
+        telemetry.addData("Status", datalog.opModeStatus);
+        telemetry.addData("Current Velocity", "%.2f ticks/sec", currentVelocity);
+        telemetry.addData("Motor Power", "%.2f", motorPower);
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        // Stop the motor when the OpMode is stopped.
+        flywheelMotor.setPower(0);
+        // The Datalogger utility automatically handles closing the file.
+        datalog.opModeStatus.set("Stopped");
+        datalog.writeLine();
+    }
+
+    /**
+     * This class encapsulates all the fields that will go into the datalog.
+     * It mirrors the Datalog class from the provided example.
+     */
+    public static class Datalog {
+        // The underlying datalogger object
+        private final Datalogger datalogger;
+
+        // These are all of the fields that we want in the datalog.
+        public Datalogger.GenericField opModeStatus = new Datalogger.GenericField("OpModeStatus");
+        public Datalogger.GenericField currentVelocity = new Datalogger.GenericField("CurrentVelocity");
+        public Datalogger.GenericField targetVelocity = new Datalogger.GenericField("TargetVelocity");
+        public Datalogger.GenericField motorPower = new Datalogger.GenericField("MotorPower");
+
+        public Datalog(String name) {
+            // Build the underlying datalog object
+            datalogger = new Datalogger.Builder()
+                    .setFilename(name)
+                    .setAutoTimestamp(Datalogger.AutoTimestamp.DECIMAL_SECONDS)
+                    .setFields(
+                            opModeStatus,
+                            currentVelocity,
+                            targetVelocity,
+                            motorPower
+                    )
+                    .build();
+        }
+
+        // Tell the datalogger to gather the values of the fields
+        // and write a new line in the log.
+        public void writeLine() {
+            datalogger.writeLine();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AprilTag/AprilTagDriveToTagOffset.java
@@ -0,0 +1,260 @@
+/* Copyright (c) 2023 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Vision.AprilTag;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This OpMode illustrates using a camera to locate and drive towards a specific AprilTag.
+ * It uses both X and Y camera offsets to continuously correct the robot's position,
+ * ensuring the front of the robot is aligned with the center of the target.
+ */
+
+@Autonomous(name="Drive To AprilTag - Continuous Offsets", group = "AprilTag")
+public class AprilTagDriveToTagOffset extends LinearOpMode
+{
+    // Adjust these numbers to suit your robot.
+    final double DESIRED_DISTANCE = 12.0; // The desired distance from the robot's front to the target (inches)
+
+    // *** IMPORTANT: You must measure these values and set them here. ***
+    // The horizontal offset from the robot's center to the camera.
+    // Positive is for a camera to the right of center, negative is for a camera to the left.
+    final double CAMERA_OFFSET_X = -3.5;
+
+    // The forward/backward offset from the robot's center to the camera.
+    // Positive is for a camera in front of center, negative is for a camera behind.
+    final double CAMERA_OFFSET_Y = 0.0;
+
+    // Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    // applied to the drive motors to correct the error.
+    final double SPEED_GAIN  =  0.02  ;   // Forward Speed Control "Gain"
+    final double STRAFE_GAIN =  0.015 ;   // Strafe Speed Control "Gain"
+    final double TURN_GAIN   =  0.01  ;   // Turn Control "Gain"
+
+    final double MAX_AUTO_SPEED = 0.5;   // Clip the approach speed to this max value (adjust for your robot)
+    final double MAX_AUTO_STRAFE= 0.5;   // Clip the strafing speed to this max value (adjust for your robot)
+    final double MAX_AUTO_TURN  = 0.3;   // Clip the turn speed to this max value (adjust for your robot)
+
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+
+    private static final boolean USE_WEBCAM = true;
+    private static final int DESIRED_TAG_ID = -1;     // Set to -1 for ANY tag.
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private AprilTagDetection desiredTag = null;
+
+    @Override public void runOpMode()
+    {
+        boolean targetFound     = false;
+        double  drive           = 0;
+        double  strafe          = 0;
+        double  turn            = 0;
+
+        initAprilTag();
+
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "motorLeftFront");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "motorRightFront");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "motorLeftBack");
+        backRightDrive = hardwareMap.get(DcMotor.class, "motorRightBack");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        if (USE_WEBCAM)
+            setManualExposure(6, 250);
+
+        telemetry.addData("Status", "Initialized - Waiting for Start");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            targetFound = false;
+            desiredTag  = null;
+
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                if (detection.metadata != null) {
+                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;
+                    }
+                }
+            }
+
+            if (targetFound) {
+                telemetry.addData("\n>","Driving to Target\n");
+                telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                telemetry.addData("Camera Range",  "%5.1f inches", desiredTag.ftcPose.range);
+                telemetry.addData("Camera Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+                telemetry.addData("Camera Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
+
+                // We now get the camera's position relative to the tag, and use our known
+                // camera offsets to find the robot's center relative to the tag.
+                // NOTE: ftcPose.x is the left-right distance, and ftcPose.y is the forward-backward distance.
+                double robotCenterX = desiredTag.ftcPose.x - CAMERA_OFFSET_X;
+                double robotCenterY = desiredTag.ftcPose.y - CAMERA_OFFSET_Y;
+
+                // Calculate the errors we need to correct.
+                double  rangeError = (robotCenterY - DESIRED_DISTANCE);
+                double  strafeError = robotCenterX;
+                double  headingError = desiredTag.ftcPose.bearing;
+
+                // Use the speed and turn "gains" to calculate the motor powers.
+                drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                strafe = Range.clip(strafeError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+
+                telemetry.addData("Robot","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+                telemetry.addData("Robot Position", "(X: %5.2f, Y: %5.2f)", robotCenterX, robotCenterY);
+
+            } else {
+                // If the target is not found, stop the robot.
+                drive = 0;
+                strafe = 0;
+                turn = 0;
+                telemetry.addData("Status", "Target Not Found - Stopping");
+            }
+            telemetry.update();
+
+            // Apply desired axes motions to the drivetrain.
+            moveRobot(drive, strafe, turn);
+            sleep(10);
+        }
+    }
+
+    /**
+     * Move robot according to desired axes motions
+     * <p>
+     * Positive X is forward
+     * <p>
+     * Positive Y is strafe left
+     * <p>
+     * Positive Yaw is counter-clockwise
+     */
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double frontLeftPower    =  x - y - yaw;
+        double frontRightPower   =  x + y + yaw;
+        double backLeftPower     =  x + y - yaw;
+        double backRightPower    =  x - y + yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+        max = Math.max(max, Math.abs(backLeftPower));
+        max = Math.max(max, Math.abs(backRightPower));
+
+        if (max > 1.0) {
+            frontLeftPower /= max;
+            frontRightPower /= max;
+            backLeftPower /= max;
+            backRightPower /= max;
+        }
+
+        // Send powers to the wheels.
+        frontLeftDrive.setPower(frontLeftPower);
+        frontRightDrive.setPower(frontRightPower);
+        backLeftDrive.setPower(backLeftPower);
+        backRightDrive.setPower(backRightPower);
+    }
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(2);
+
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam1"))
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+
+    /**
+     * Manually set the camera gain and exposure.
+     * This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+     */
+    private void setManualExposure(int exposureMS, int gain) {
+        if (visionPortal == null) {
+            return;
+        }
+
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        if (!isStopRequested()) {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+        }
+    }
+}
