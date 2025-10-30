ca4116f1ead8cc1d54b0f21cb9dc0aac51e109e6
baqwas <baqwas@gmail.com>
2025-10-30 11:44:17 -0500

    Missing import statement in BangBangController and ToLine_Linear (retired)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/retired/Auto/ToLine_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/retired/Auto/ToLine_Linear.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.Holonomic;
+package org.firstinspires.ftc.teamcode.retired.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
